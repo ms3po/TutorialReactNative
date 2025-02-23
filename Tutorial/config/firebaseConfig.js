ccffd1e0698fdc67e9b91f70bb7aeeb8527fd36b
Ms3po <toomschoth@gmail.com>
2025-02-23 14:55:25 +0100

    Delete

--- Tutorial/config/firebaseConfig.js
+++ Tutorial/config/firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { initializeAuth , getReactNativePersistence} from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore"; // Import getFirestore
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
